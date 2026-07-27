beb4fe8d560bf2a7219cf0ef1c5220f5edbe8207
Natnael <natnaelatnafu25@gmail.com>
2026-07-26 21:18:54 -0500

    part 4: no_vowels function to remove vowels from a string EX

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,21 @@
 # Write your solution here
+
+def no_vowels(my_string):
+    vowels = "aeiou"
+    for i in my_string:
+
+        if i in vowels:
+           my_string = my_string.replace(i, "")
+           
+    return my_string
+
+
+
+
+if __name__ == "__main__":
+    my_string = "xzcvb"
+    print(no_vowels(my_string))
+
+
+
+
