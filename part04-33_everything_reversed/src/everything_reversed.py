ddffb27cfbe94b63ad2ab4cfbf0c2b14bc0f6fc5
Natnael <natnaelatnafu25@gmail.com>
2026-07-26 20:26:46 -0500

    Part 4: strings and lists

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def everything_reversed(my_list: list[str]):
+
+    new_list = []
+    word = ""
+    for i in my_list:
+        word = i[::-1]
+        new_list.append(word)
+    new_list = new_list[::-1]
+
+    return new_list
+
+
+
+if __name__ == '__main__':
+    my_list = ["Hi", "there", "example", "one more"]
+    print(everything_reversed(my_list))
