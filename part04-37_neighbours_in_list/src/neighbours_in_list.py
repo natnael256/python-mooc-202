09672af732f4a551d155425d79970ee9da74115f
Natnael <natnaelatnafu25@gmail.com>
2026-07-28 23:29:58 -0500

    part 4: working last section of part 4

--- part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+def longest_series_of_neighbours(my_list):
+    longest = 0
+
+
+    for i in my_list:
+        currnt_num = i 
+        next_num = [i + 1]
+
+        if abs(currnt_num - next_num) == 1:
+            longest += longest
+
+    return longest  
+            
+my_list = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
+print(longest_series_of_neighbours(my_list))
