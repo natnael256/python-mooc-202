0b2c041ce76bd95836ade579bba933518a157e91
Natnael <natnaelatnafu25@gmail.com>
2026-08-06 11:46:29 -0500

    Implement functions to find longest string, count matching elements, and validate Sudoku rows and columns

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,17 @@
 # Write your solution here
+
+
+
+def longest(my_list: []):
+
+    longest_word = ""
+
+    for i in my_list:
+        if len(i) > len(longest_word):
+            longest_word = i
+    return longest_word
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
--- part05-05_sudoku_column/src/sudoku_column.py
+++ part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int):
+    
+    num = []
+
+    for row in sudoku:
+
+        if row[column_no] == 0:
+            num.append(row[column_no])
+            continue
+        if row[column_no] in num:
+            return False
+        else:
+            num.append(row[column_no])
+    return True
+
+    
