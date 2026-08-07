efa41b9e7da7257bf3e4f28dc154bf3f07103ea9
Natnael <natnaelatnafu25@gmail.com>
2026-08-07 13:07:00 -0500

    Add implementation of double_items function to double list values

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,24 @@
 # Write your solution here
+# Please write a function named double_items(numbers: list), which takes a list of integers as its argument.
+
+# The function should return a new list, which contains all values from the original list doubled. The function should not change the original list.
+
+# An example of the function at work:
+
+
+def double_items(numbers: list):
+
+    new_list = []
+
+    for item in numbers:
+
+        new_list.append(item * 2)
+    return new_list
+
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
