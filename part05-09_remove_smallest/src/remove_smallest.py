d4ca43b2931ac05ec39759d4ed5104bdb90daafe
Natnael <natnaelatnafu25@gmail.com>
2026-08-07 22:05:49 -0500

    remove_smallest function and add Sudoku grid printing and number addition

--- part05-09_remove_smallest/src/remove_smallest.py
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+
+def remove_smallest(numbers):
+
+    smallest = numbers.index(min(numbers))
+    numbers.pop(smallest)
+
+    
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
